e882263e00928f73c0073953852e7cb61c9f67f3
Michael Krämer <mkraemer@postlab.de>
2015-05-10 11:51:46 +0200

    implements Plot 4

--- plot4.R
+++ plot4.R
@@ -12,11 +12,27 @@ mydata <- transform(mydata, dDate=as.Date(dDate, "%d/%m/%Y"),
                     dTime=strptime(paste(dDate, dTime), "%d/%m/%Y %T"))
 
 # create file and plot to it
-png(file="plot3.png")
+png(file="plot4.png")
+par(mfrow = c(2, 2))
+
+#top left
+with(mydata, plot(dTime, Global_active_power, type="n", xlab="", ylab="Global Active Power"))
+with(mydata, lines(dTime, Global_active_power))
+
+#top right
+with(mydata, plot(dTime, Voltage, type="n", xlab="datetime"))
+with(mydata, lines(dTime, Voltage))
+
+#bottom left
 with(mydata, plot(dTime, Sub_metering_1, type="n", xlab="", ylab="Energy sub metering"))
 with(mydata, lines(dTime, Sub_metering_1, col="black"))
 with(mydata, lines(dTime, Sub_metering_2, col="red"))
 with(mydata, lines(dTime, Sub_metering_3, col="blue"))
-legend("topright", lwd=1, col=c("black", "red", "blue"),
+legend("topright", lwd=1, bty="n", col=c("black", "red", "blue"),
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+#bottom right
+with(mydata, plot(dTime, Global_reactive_power, type="n", xlab="datetime"))
+with(mydata, lines(dTime, Global_reactive_power))
+
 dev.off()
